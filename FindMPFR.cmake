70f7e3aab4a8148d772c222f6fd9028856f153e5
Georgios Kafanas <g.kafanas@greeklug.gr>
2023-04-06 16:57:10 +0300

    BUG_FIX: Mark correct library variables for MPFR

--- FindMPFR.cmake
+++ FindMPFR.cmake
@@ -1,12 +1,12 @@
 # Find MPFR
 
-find_library(MPFR_LIBRARY mpfr)
-find_path(MPFR_INCLUDE_DIR mpfr.h)
+find_library(MPFR_LIBRARIES mpfr)
+find_path(MPFR_INCLUDE_DIRS mpfr.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MPFR DEFAULT_MSG MPFR_LIBRARY MPFR_INCLUDE_DIR)
+find_package_handle_standard_args(MPFR DEFAULT_MSG MPFR_LIBRARIES MPFR_INCLUDE_DIRS)
 
 mark_as_advanced(
-	ARIADNE_INCLUDE_DIRS
-	ARIADNE_LIBRARIES
+	MPFR_INCLUDE_DIRS
+	MPFR_LIBRARIES
 )
