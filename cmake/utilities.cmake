a8122da61c043c8eae9091feff33902a8372b273
Georgios Kafanas <g.kafanas@greeklug.gr>
2023-04-09 13:29:15 +0300

    Handle single configuration generators

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -1,7 +1,10 @@
-function(set_output_dir_structure)
+macro(set_output_dir_structure_single_config)
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}" PARENT_SCOPE)
+	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}" PARENT_SCOPE)
+	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}" PARENT_SCOPE)
+endmacro()
 
-	include(GNUInstallDirs)
-
+macro(set_output_dir_structure_multiple_config)
 	foreach(CONFIG ${CMAKE_CONFIGURATION_TYPES})
 		string(TOUPPER "${CONFIG}" CONFIG_SUFFIX)
 
@@ -9,5 +12,16 @@ function(set_output_dir_structure)
 		set("CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONFIG_SUFFIX}" "${CMAKE_BINARY_DIR}/${CONFIG}/${CMAKE_INSTALL_LIBDIR}" PARENT_SCOPE)
 		set("CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${CONFIG_SUFFIX}" "${CMAKE_BINARY_DIR}/${CONFIG}/${CMAKE_INSTALL_LIBDIR}" PARENT_SCOPE)
 	endforeach()
+endmacro()
 
+function(set_output_dir_structure)
+	include(GNUInstallDirs)
+
+	get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
+	if(isMultiConfig)
+		set_output_dir_structure_multiple_config()
+	else()
+		set_output_dir_structure_single_config()
+	endif()
 endfunction()
